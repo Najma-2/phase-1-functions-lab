ea977152fdfc5c2f3958fbb6976c79e0e8238ebf
Najma <najma.a16@gmail.com>
2022-11-01 10:41:29 -0500

    commit tree cfb5373fba8938675fbd3e488727e8a680c49dd7

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    if (someValue<42){
+      return(42-someValue);
+    }
+    else {
+  
+    return(someValue-42)
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceTravelledInFeet(start, dest) {
+    if (start > dest) {
+      let distance = start - dest; 
+      return distance * 264;
+    }
+    else if (start < dest) {
+      let distance = dest - start; 
+      return distance * 264;
+    }
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    const dist = distance - 400;
+    if (distance < 400){
+        return 0
+    }
+    else if (distance > 400 && distance < 2000){
+        return dist * 0.02
+    }
+    else if (distance > 2000 && distance < 2500) {
+        return 25; 
+    }
+    else {
+         return "cannot travel that far"; 
+    }   
+}
